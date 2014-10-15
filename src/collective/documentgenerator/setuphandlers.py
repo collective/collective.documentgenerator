f3efc678681089ff9b1593e6f00bfeacb2427532
Vivian Antoine <vivian@DieKatze.(none)>
2014-10-15 09:38:16 +0200

    Add the new demo profile with an upgrade step.

--- src/collective/documentgenerator/setuphandlers.py
+++ src/collective/documentgenerator/setuphandlers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
+
 
 def isNotCurrentProfile(context):
     return context.readDataFile("collectivedocumentgenerator_marker.txt") is None
@@ -9,3 +11,13 @@ def post_install(context):
     """Post install script"""
     if isNotCurrentProfile(context): return
     portal = context.getSite()
+
+
+def install_demo(context):
+    """ """
+    portal = api.portal.get()
+    templates_folder = api.content.create(
+        type='Folder',
+        title='podtemplates',
+        container=portal
+    )
--- src/collective/documentgenerator/tests/test_demo_profile.py
+++ src/collective/documentgenerator/tests/test_demo_profile.py
@@ -3,6 +3,7 @@
 from collective.documentgenerator.testing import TEST_INSTALL_INTEGRATION
 
 from plone import api
+from plone.app.testing import applyProfile
 
 import unittest
 
@@ -17,3 +18,15 @@ class TestDemoProfile(unittest.TestCase):
         profile_ids = [info['id'] for info in portal_setup.listProfileInfo()]
         msg = 'demo profile is not registered'
         self.assertTrue(demo_profile_name in profile_ids, msg)
+
+    def test_PODTemplate_folder_creation(self):
+        site = api.portal.get()
+
+        msg = 'Pod templates folder should not exist'
+        self.assertTrue(not hasattr(site, 'podtemplates'), msg)
+
+        applyProfile(site, 'collective.documentgenerator:demo')
+
+        msg = 'Pod templates folder doesn\'t exist'
+        self.assertTrue(hasattr(site, 'podtemplates'), msg)
+        self.assertTrue(site.podtemplates.portal_type == 'Folder')
