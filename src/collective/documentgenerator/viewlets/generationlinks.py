1f2509654fcdff4a418002b75f681ee2aa831ba2
Vivian Antoine <vivian@DieKatze.(none)>
2014-10-15 15:30:26 +0200

    Complete the viewlet generation.

--- src/collective/documentgenerator/viewlets/generationlinks.py
+++ src/collective/documentgenerator/viewlets/generationlinks.py
@@ -2,7 +2,11 @@
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from collective.documentgenerator.content.pod_template import IPODTemplate
+
+from plone import api
 from plone.app.layout.viewlets import ViewletBase
+
 from zope.component import getMultiAdapter
 # ------------------------------------------------------------------------------
 
@@ -24,3 +28,28 @@ class DocumentGeneratorLinksViewlet(ViewletBase):
 
     def getPortalUrl(self):
         getToolByName(self.context, 'portal_url').getPortalPath()
+
+    def getAllModels(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        brains = catalog(object_provides=IPODTemplate.__identifier__)
+        pod_templates = [brain.getObject() for brain in brains]
+        return pod_templates
+
+    def getGenerableModels(self):
+        pod_templates = self.getAllModels()
+        generable_templates = [pt for pt in pod_templates if pt.can_be_generated(self.context)]
+        return generable_templates
+
+    def getLinksInfo(self):
+        base_url = self.context.absolute_url()
+        generable_templates = self.getGenerableModels()
+        links = []
+        for template in generable_templates:
+            title = template.Title()
+            uid = template.UID()
+            link = '{base_url}/document-generation?doc_uid={uid}'.format(
+                base_url=base_url,
+                uid=uid,
+            )
+            links.append({'link': link, 'title': title})
+        return links
