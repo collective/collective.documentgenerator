a722fd266c16173ca2ed44be87a76520ce226a6c
Vivian Antoine <vivian.antoine@imio.be>
2014-11-26 16:35:43 +0100

    Add a new helper method.

--- src/collective/documentgenerator/demo/helper.py
+++ src/collective/documentgenerator/demo/helper.py
@@ -8,5 +8,15 @@ class DemoHelperView(ATDocumentGenerationHelperView):
     Archetypes implementation of document generation helper methods.
     """
 
-    def test(self):
-        import ipdb; ipdb.set_trace()
+    def get_default_CT_fields(self):
+        field_list = []
+        filtered_fields = ['id', 'title', 'query', 'sort_on', 'sort_reversed', 'b_size',
+                           'limit', 'customViewFields']
+        default_field_list = self.real_context.schema.getSchemataFields('default')
+        for field in default_field_list:
+            if field.getName() not in filtered_fields:
+                field_list.append(field.getName())
+        return field_list
+
+    def is_rich_text_field(self, field_name):
+        return self.real_context.schema.get(field_name).getWidgetName() == 'RichWidget'
