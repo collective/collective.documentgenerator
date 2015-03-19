a3529d12b236c684d6853dfd240ac2e9a9ef97bc
Vivian Antoine <vivian.antoine@imio.be>
2015-03-19 09:48:54 +0100

    Factorize "create_temporary_file" to imio.pyutils

--- src/collective/documentgenerator/events/styles_events.py
+++ src/collective/documentgenerator/events/styles_events.py
@@ -4,6 +4,8 @@ from appy.shared.utils import executeCommand
 
 from collective.documentgenerator.content.pod_template import IPODTemplate
 
+from imio.pyutils.system import create_temporary_file
+
 from plone import api
 from plone.namedfile.file import NamedBlobFile
 
@@ -24,7 +26,10 @@ def update_styles_of_all_PODtemplate(style_template, event):
     """
     style_odt = style_template.odt_file
     #template style is modify, update all template with style.
-    style_template_filename = _create_temporary_style_template(style_odt)
+    style_template_filename = create_temporary_file(
+        style_odt,
+        'style_template'
+    )
     if style_template_filename:
         catalog = api.portal.get_tool('portal_catalog')
         pod_templates = catalog(object_provides=IPODTemplate.__identifier__)
@@ -46,23 +51,11 @@ def update_PODtemplate_styles(pod_template, event):
     if not style_template:
         return
     style_odt = style_template.odt_file
-    style_template_filename = _create_temporary_style_template(style_odt)
+    style_template_filename = create_temporary_file(
+        style_odt,
+        'style_template'
+    )
     _update_template_style(pod_template, style_template_filename)
-
-
-def _create_temporary_style_template(style_template):
-    """
-    Create Temporary file from template style.
-    """
-    if style_template and style_template.size:
-        #save in temporary file, the style_template
-
-        style_template_filename = '%s/%s_%f.%s' % (tempfile.gettempdir(), 'style_template', time.time(), 'odt')
-        new_style_template = file(style_template_filename, "w")
-        new_style_template.write(style_template.data)
-        new_style_template.close()
-        return style_template_filename
-    return ''
 
 
 def _update_template_style(pod_template, style_template_filename):
@@ -70,17 +63,14 @@ def _update_template_style(pod_template, style_template_filename):
     Update template pod_template by templateStyle.
     """
     #save in temporary file, the template
-    tempFileName = '%s/%s_%f.%s' % (tempfile.gettempdir(), pod_template.UID(), time.time(), 'odt')
-    newTemplate = file(tempFileName, "w")
-    newTemplate.write(pod_template.odt_file.data)
-    newTemplate.close()
+    tempFileName = create_temporary_file(pod_template.odt_file, 'pod_template')
     #merge style from templateStyle in template
     cmd = '%s %s %s %s -p%d -t%s' % \
         ('/usr/bin/python', CONVSCRIPT, tempFileName, 'odt',
             2002, style_template_filename)
     executeCommand(cmd)
     #read the merged file
-    resTempFileName = tempFileName[:-3] + 'res.odt'
+    resTempFileName = tempFileName.split('.')[0] + '.odt'
     if os.path.isfile(resTempFileName):
         resTemplate = open(resTempFileName, 'rb')
         #update template
