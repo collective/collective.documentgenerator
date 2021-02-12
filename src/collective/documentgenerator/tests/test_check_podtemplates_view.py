42e9b27cae44bc6429cbacc68a635ced6e5f6694
Olivier Delaere <23073839+odelaere@users.noreply.github.com>
2021-02-12 10:27:53 +0100

    improved test_view_check_pod_templates_message core improved test_view_check_pod_templates_message core

--- src/collective/documentgenerator/tests/test_check_podtemplates_view.py
+++ src/collective/documentgenerator/tests/test_check_podtemplates_view.py
@@ -12,11 +12,21 @@ class TestCheckPodTemplatesView(PODTemplateIntegrationTest):
     def setUp(self):
         super(TestCheckPodTemplatesView, self).setUp()
 
-    def test_view_check_pod_templates_message(self):
-        check_view = self.portal.restrictedTraverse("@@check-pod-templates")
-        check_view()
-        messages = check_view.messages
+    def _assert_messages_format(self, check_view, nb_clean, nb_error=0, nb_no_obj_found=0,
+                                nb_no_pod_portal_types=0, nb_not_enabled=0, nb_not_managed=0):
+        def _assert_fmt(lst, length):
+            self.assertEqual(len(lst), length)
+            for element in lst:
+                self.assertGreaterEqual(len(element), 2)
+                self.assertIsInstance(element[0], PODTemplate)
+                self.assertNotIsInstance(element[1], PODTemplate)
+                if len(element) > 2:
+                    self.assertEqual(len(element), 3)
+                    self.assertEqual(len(element[-1]), 2)
+                    self.assertEqual(element[-1][0], _("Error"))
+                    self.assertIsInstance(element[-1][1], str)
 
+        messages = check_view.messages
         self.assertEqual(len(messages), 6)
         self.assertEqual(len(check_view.left_to_verify), 0)
         self.assertListEqual(messages[_("check_pod_template_error")], check_view.error)
@@ -35,28 +45,37 @@ class TestCheckPodTemplatesView(PODTemplateIntegrationTest):
         )
         self.assertListEqual(messages[_("check_pod_template_clean")], check_view.clean)
 
-        self.assertGreaterEqual(len(check_view.error), 1)
-        for error in check_view.error:
-            self.assertEqual(len(error), 3)
-            self.assertIsInstance(error[0], PODTemplate)
-            self.assertNotIsInstance(error[1], PODTemplate)
-            self.assertEqual(len(error[-1]), 2)
-            self.assertEqual(error[-1][0], _("Error"))
-            self.assertIsInstance(error[-1][1], str)
+        _assert_fmt(check_view.clean, nb_clean)
+        _assert_fmt(check_view.error, nb_error)
+        _assert_fmt(check_view.no_obj_found, nb_no_obj_found)
+        _assert_fmt(check_view.no_pod_portal_types, nb_no_pod_portal_types)
+        _assert_fmt(check_view.not_enabled, nb_not_enabled)
+        _assert_fmt(check_view.not_managed, nb_not_managed)
 
-        self.assertEqual(len(check_view.no_obj_found), 0)
-        self.assertEqual(len(check_view.no_pod_portal_types), 0)
-        self.assertEqual(len(check_view.not_enabled), 0)
-        self.assertEqual(len(check_view.not_managed), 0)
-
-        self.assertGreaterEqual(len(check_view.clean), 8)
-        for clean in check_view.clean:
-            self.assertEqual(len(clean), 2)
-            self.assertIsInstance(clean[0], PODTemplate)
-            self.assertNotIsInstance(clean[1], PODTemplate)
-
+    def test_view_check_pod_templates_message(self):
         # assert every pod templates are checked at least once (SubTemplates may be referenced multiple times)
         catalog = api.portal.get_tool("portal_catalog")
         brains = catalog(object_provides=IPODTemplate.__identifier__)
-        total_nb_checked = len(check_view.error) + len(check_view.clean)
-        self.assertGreaterEqual(total_nb_checked, len(brains))
+        self.assertEqual(len(brains), 9)
+
+        check_view = self.portal.restrictedTraverse("@@check-pod-templates")
+        check_view()
+        # default everything should be clean
+        self._assert_messages_format(check_view, 10)
+
+        # todo implement these tests
+        # test error
+        # check_view()
+        # self._assert_messages_format(check_view, 9, nb_error=1)
+        # # test no_obj_found
+        # check_view()
+        # self._assert_messages_format(check_view, 9, nb_no_obj_found=1)
+        # # test no_pod_portal_types
+        # check_view()
+        # self._assert_messages_format(check_view, 9, nb_no_pod_portal_types=1)
+        # # test not_enabled
+        # check_view()
+        # self._assert_messages_format(check_view, 9, nb_not_enabled=1)
+        # # test not_managed
+        # check_view()
+        # self._assert_messages_format(check_view, 9, nb_not_managed=1)
