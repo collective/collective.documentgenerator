13960da6189ff2d309f4aacca3aca64a115f969d
Gauthier Bastien <gauthier.bastien@imio.be>
2022-01-14 13:38:41 +0100

    Added License classifier

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: Addon",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
     ],
